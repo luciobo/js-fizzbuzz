d78f6587ce0da7792a3a435f626eb8a5c951a75f
luciobo <luciobo@icloud.com>
2022-11-02 16:43:41 +0100

    completata formula base, con errore

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 
 
-for (let i = 0; i <= 100; i++) {
-    console.log(i)
+for (let i = 1; i <= 100; i++) {
+    // console.log(i)
 
-    if (i = ) {
-
-    } 
+    if (i % 3 === 0) {
+        console.log("fizz")
+    } else if (i % 5 === 0) {
+        console.log("buzz")
+    } else if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzBuzz")
+    } else {
+        console.log(i)
+    }
 
 }
