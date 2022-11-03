1ae51cc5af2111824c563d312963934e67bd23c4
luciobo <luciobo@icloud.com>
2022-11-03 10:19:36 +0100

    correzione

--- js/main.js
+++ js/main.js
@@ -3,19 +3,16 @@
 for (let i = 1; i <= 100; i++) {
     // console.log(i)
 
+
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("fizzBuzz")
-    } else if (i % 3 === 0) {
-        console.log("fizz")
     } else if (i % 5 === 0) {
         console.log("buzz")
+    } else if (i % 3 === 0) {
+        console.log("fizz")
     } else {
         console.log(i)
     }
-
-
-
-
 
 
 }
